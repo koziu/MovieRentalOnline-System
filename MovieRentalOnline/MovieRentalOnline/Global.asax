aa468125d20057b7d1ba2f1e1e002d90ae9419d3
Mateusz Kozak <koziu92@gmail.com>
2015-03-31 13:30:16 +0200

    MVC, UnitTests

---
+++ MovieRentalOnline/MovieRentalOnline/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieRentalOnline.MvcApplication" Language="C#" %>
---
+++ MovieRentalOnline/MovieRentalOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieRentalOnline.Startup))]
+namespace MovieRentalOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
