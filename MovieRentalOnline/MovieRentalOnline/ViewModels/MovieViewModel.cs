92a19d5241d8c4ac680f0d4db618d82776713c28
Mateusz Kozak <koziu92@gmail.com>
2015-05-25 20:21:07 +0200

    MovieViewModel - pusty

---
+++ MovieRentalOnline/MovieRentalOnline/ViewModels/MovieViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieRentalOnline.ViewModels
+{
+    public class MovieViewModel
+    {
+        
+    }
+}
